1ade0006c286ca1a98cd0714f3d7f097aa1f6181
sancodes <munsan14@gmail.com>
2021-06-27 10:27:05 -0700

    add variable changes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,10 +7,10 @@ import { formatDate } from '../utils';
 
 import styles from './Header.styles';
 
-const getColumns = (numberOfDays, selectedDate) => {
+const datesFrom = (nDays, selectedDate) => {
   const columns = [];
   let initial = 0;
-  for (let i = initial; i < (numberOfDays + initial); i += 1) {
+  for (let i = initial; i < (nDays + initial); i += 1) {
     let date = moment(selectedDate);
     date = date.add(i, 'd');
     columns.push(date.toDate());
@@ -18,55 +18,48 @@ const getColumns = (numberOfDays, selectedDate) => {
   return columns;
 };
 
-const getFontSizeHeader = (numberOfDays) => {
-  if (numberOfDays > 1) {
+const headerFontSize = (nDays) => {
+  if (nDays > 1) {
     return 12;
   }
 
   return 16;
 };
 
-const getDayTextStyles = (numberOfDays) => {
-  const fontSize = numberOfDays === 6 ? 10 : 12;
+const dayTextStyle = (nDays) => {
+  const fontSize = nDays === 6 ? 10 : 12;
   return {
     fontSize,
   };
 };
 
 const Column = ({
-  column, numberOfDays, format,
+  column, nDays, format,
 }) => {
   return (
     <View style={styles.column}>
-      <Text style={[styles.text, getDayTextStyles(numberOfDays)]}>
+      <Text style={[styles.text, dayTextStyle(nDays)]}>
         {formatDate(column, format)}
       </Text>
     </View>
   );
 };
 
-const Columns = ({ columns, numberOfDays, format }) => {
+const DaysHeader = ({ columns, nDays, format }) => {
   return (
     <View style={styles.columns}>
       {columns.map((column) => {
-        return (
-          <Column
-            key={column}
-            column={column}
-            numberOfDays={numberOfDays}
-            format={format}
-          />
-        );
+        return
       })}
     </View>
   );
 };
 
-const Title = ({ numberOfDays }) => {
+const Title = ({ nDays }) => {
   return (
     <View style={styles.title}>
       <Text
-        style={[styles.text, { fontSize: getFontSizeHeader(numberOfDays) }]}
+        style={[styles.text, { fontSize: headerFontSize(nDays) }]}
       >
 
       </Text>
@@ -75,26 +68,26 @@ const Title = ({ numberOfDays }) => {
 };
 
 const WeekViewHeader = ({
-  numberOfDays, selectedDate, formatDate, style,
+  nDays, selectedDate, dateFormat, style,
 }) => {
-  const columns = getColumns(numberOfDays, selectedDate);
+  const columns = datesFrom(nDays, selectedDate);
   return (
     <View style={[styles.container, style]}>
-      <Title numberOfDays={numberOfDays} selectedDate={selectedDate} />
-      {columns && <Columns format={formatDate} columns={columns} numberOfDays={numberOfDays} />}
+      <Title nDays={nDays} selectedDate={selectedDate} />
+      {columns && <Columns format={dateFormat} columns={columns} nDays={nDays} />}
     </View>
   );
 };
 
 WeekViewHeader.propTypes = {
-  numberOfDays: PropTypes.oneOf([1, 3, 5, 6, 7]).isRequired,
+  nDays: PropTypes.oneOf([1, 3, 5, 6, 7]).isRequired,
   selectedDate: PropTypes.instanceOf(Date).isRequired,
-  formatDate: PropTypes.string,
+  dateFormat: PropTypes.string,
   style: PropTypes.object,
 };
 
 WeekViewHeader.defaultProps = {
-  formatDate: 'dddd', // day of week
+  dateFormat: 'dddd', // day of week
 };
 
 export default WeekViewHeader;
